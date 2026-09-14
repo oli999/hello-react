7909a7152b3ff691eee4c03a74e4c0c9aa2d54d3
kimgura <oli999@naver.com>
2026-09-14 10:32:13 +0900

    예제 추가함

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {  // 프록시할 경로
+        target: 'http://localhost:3000', // 백엔드 서버 주소
+        changeOrigin: true, 
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''), // '/api' 제거 
+      }
+    }
+  }
 })
